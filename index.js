0d5db8852b27703dfe20071999e050acc68ecbac
kkoomin <minha.koo.9@gmail.com>
2019-02-17 23:50:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(arr, str) {
+    return arr.filter(function (name) { return str.toLowerCase() === name.toLowerCase(); });
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(function (name) { return name.slice(0, str.length) === str })
+}
+
+function matchName(arr, str) {
+    return arr.filter(function (driver) { return driver.name.toLowerCase() === str.toLowerCase(); })
+}
+
